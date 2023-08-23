9fe9233ee2ccb6db6373a6e0792407d18d52557a
Kenji <fcb141021@yahoo.co.jp>
2023-08-23 17:02:50 +0900

    プロフィール画像の編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to pictures_path, notice: "プロフィールを編集しました！"
+      redirect_to pictures_path, notice: "プロフィールを登録しました！"
     else
       render :edit
     end
